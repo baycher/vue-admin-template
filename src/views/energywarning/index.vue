4b13afe9a8bdb0bd7920ac59b1759747e6e4dbf2
baycher <baycher@163.com>
2025-04-12 15:48:47 +0800

    https://gitee.com/baycher/carbon-admin-web.git

---
+++ src/views/energywarning/index.vue
@@ -0,0 +1,18 @@
+<template>
+  
+
+
+  
+</template>
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+  computed: {
+    ...mapGetters([
+      'name'
+    ])
+  }
+}
+</script>
